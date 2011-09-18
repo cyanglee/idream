98bd8750c2e4000504cdee74b588c9c28979725a
Kenneth Lee <cyoung.techs@gmail.com>
2011-09-18 12:07:24 -0400

    fixed #5. This was a Devise related issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
   #skip_authorize_resource :only => [:edit, :update]
-  before_filter :find_user, :only => [:show, :edit, :update, :show_schools, :associate_schools]
+  before_filter :find_user, :only => [:show, :edit, :update]
   #before_filter :filter_roles, :only => [:show, :edit, :update]
 
   #before_filter :authenticate_user!#, :except => [:show, :index]
@@ -21,18 +21,14 @@ class UsersController < ApplicationController
     # TODO: figure out if there is a better way than using find_by_email to increase the performance
     user_to_update = if !target_user[:email].nil? and @user.email != target_user[:email] then User.find_by_email(target_user[:email]) else @user end
     if user_to_update.update_attributes(params[:user])
-      redirect_to @user, :notice => "Successfully updated user."
+      # This is related to issue #5. Devise logs off the login user after the password change.
+      # This makes the User object null and that's why the Cancan error message comes out.
+      # The solution is to sign in the user again before the redirect.
+      sign_in(user_to_update, :bypass => true)
+      redirect_to user_to_update, :notice => "Successfully updated user."
     else
       render :action => 'edit'
     end
-  end
-
-  def show_schools
-    render 'show_schools'
-  end
-
-  def associate_schools
-    render 'associate_schools'
   end
 
   private
@@ -41,15 +37,5 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
-  def filter_roles
-    @filtered_roles = []
-    for role in Role.all
-      if can? :manage, @role
-        @filtered_roles << role
-      else
-        @filtered_roles << role if role.name != 'Admin'
-      end
-    end
-  end
 end
 
